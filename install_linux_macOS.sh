a8afda430bbe429f6ec9cbddb775a22be4040c14
Markus Hollander <markus-hollander@web.de>
2020-02-13 14:47:06 +0100

    testing linux remove directory

--- install_linux_macOS.sh
+++ install_linux_macOS.sh
@@ -1,8 +1,8 @@
 DIR="build/"
 if [ -d "$DIR" ]; then
   rm -rf "$DIR"
-  mkdir "$DIR"
 fi
+mkdir "$DIR"
 cmake -S . -B "$DIR"
 cmake --build "$DIR" --config Release --target install
 chmod +x propores
